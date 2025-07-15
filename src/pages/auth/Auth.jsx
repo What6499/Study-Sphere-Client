e476d500c4f0609f4e23daf2ba8bbb335b57abdb
Jamil Ahmed <what6499@gmail.com>
2025-07-15 16:15:28 +0600

    setting up navbar

---
+++ src/pages/auth/Auth.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Auth = () => {
+  return <div className="blur-2xl"></div>;
+};
+
+export default Auth;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import Auth from "../pages/auth/Auth";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +13,20 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
+      },
+      {
+        path: "auth",
+        Component: Auth,
+        children: [
+          {
+            path: "login",
+            Component: Login,
+          },
+          {
+            path: "register",
+            Component: Register,
+          },
+        ],
       },
     ],
   },
